668a444d2bd290e2c06f701249af1757983013df
devinlinux <102993661+devinlinux@users.noreply.github.com>
2022-08-04 15:14:07 -0400

    Update consolecalc.js

--- pages/works/consolecalc.js
+++ pages/works/consolecalc.js
@@ -11,14 +11,14 @@ const Work = () => {
 					Console Calculator <Badge>2022</Badge>
 				</Title>
 				<P>
-					A basic Windows Console Calculator written in C++ using Visual Studio. The calculator was made to run on any device running the Windows operating system.
+					A basic Windows Console Calculator written in C++ using Visual Studio. The calculator was made to run on any device running the Windows operating system. Amy messages saying that this file is malicious is most likely a false flag. Even so, the choice is entirely your on whether or not to keep the download and run the program. 
 				</P>
 				<List ml={4} my={4}>
 					<ListItem>
 						<Meta>
 							Download
 						</Meta>
-						<Link href="https://michaelbobrowski.000webhostapp.com">
+						<Link href="https://michaelbobrowski.000webhostapp.com/projects/cpp/consolecalc/consolecalc.zip">
 							Download
 						</Link>	
 					</ListItem>

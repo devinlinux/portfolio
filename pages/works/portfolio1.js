48a8c21c9f09984930d1b6b4dc5bee59f2dfed58
devinlinux <102993661+devinlinux@users.noreply.github.com>
2022-08-04 14:29:06 -0400

    Add files via upload

---
+++ pages/works/portfolio1.js
@@ -0,0 +1,38 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExtenralLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import Layout from '../../components/layouts/article'
+import P from '../../components/paragraph'
+
+const Work = () => {
+	return (
+		<Layout title="Portfolio">
+			<Container>
+				<Title>
+					My Original Portfolio <Badge>2022</Badge>
+				</Title>
+				<P>
+					The original portfolio that I made for a summer class. Coded entirely in HTML, CSS, JavaScript and PHP. The site is not reactive and does not make use of any JavaScript libraries. 
+				</P>
+				<List ml={4} my={4}>
+					<ListItem>
+						<Meta>
+							Visit
+						</Meta>
+						<Link href="https://michaelbobr.000webhostapp.com">
+							michaelbobr.000webhostapp.com
+						</Link>
+					</ListItem>
+					<ListItem>
+						<Meta>
+							Platform
+						</Meta>
+						<span>Any Platform that has a Web Browser</span>
+					</ListItem>
+				</List>
+			</Container>
+		</Layout>
+	)
+}
+
+export default Work

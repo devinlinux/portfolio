936e694b3f7b3924c79b1c379eba65ec8aabdfd0
devinlinux <102993661+devinlinux@users.noreply.github.com>
2022-08-30 10:38:31 -0400

    Update index.js
    Corrected url

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ const Home = () => (
           Michael is a sophomore at Bishop Ireton High School in Alexandria, Virginia. He enjoys making websites and applications using several different programming languages. When not developing online, he plays Baseball and Swims for Bishop Ireton High School. During his sophomore year of high school he will be taking AP Computer Science Applications to further his knowledge of Computer Science Topics.
         </Paragraph>
         <Box align="center" my={4}>
-          <NextLink href="https://michaelbobrowski.000webhostapp.com" passHref scroll={false}>
+          <NextLink href="/works" passHref scroll={false}>
             <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
               My portfolio
             </Button>
